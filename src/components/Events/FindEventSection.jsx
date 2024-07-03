3690e0de68ca20591221213e8a8d9555c732c5e1
Oleg Zalozhenkov <oleg.zalozhenkov@gmail.com>
2024-07-03 12:37:28 +0200

    393. Dynamic Query Functions & Query Keys

--- src/components/Events/FindEventSection.jsx
+++ src/components/Events/FindEventSection.jsx
@@ -1,26 +1,60 @@
-import { useRef } from 'react';
+import {useRef, useState} from 'react';
+import {useQuery} from "@tanstack/react-query";
+import {fetchEvents} from "../../util/http.js";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+import EventItem from "./EventItem.jsx";
 
 export default function FindEventSection() {
-  const searchElement = useRef();
+    const searchElement = useRef();
+    const [searchTerm, setSearchTerm] = useState()
+    const {data, isPending, isError, error} = useQuery({
+        queryKey: ['events', {search: searchTerm}],
+        queryFn: () => fetchEvents(searchTerm)
+    })
 
-  function handleSubmit(event) {
-    event.preventDefault();
-  }
+    function handleSubmit(event) {
+        event.preventDefault();
+        setSearchTerm(searchElement.current.value)
+    }
 
-  return (
-    <section className="content-section" id="all-events-section">
-      <header>
-        <h2>Find your next event!</h2>
-        <form onSubmit={handleSubmit} id="search-form">
-          <input
-            type="search"
-            placeholder="Search events"
-            ref={searchElement}
-          />
-          <button>Search</button>
-        </form>
-      </header>
-      <p>Please enter a search term and to find events.</p>
-    </section>
-  );
+    let content = <p>Please enter a search term and to find events.</p>;
+    if (isPending) {
+        content = <LoadingIndicator/>
+    }
+    if (isError) {
+        content = <ErrorBlock
+            title='An error occured'
+            message={error.info?.message || 'Failed to fetch events'}
+        />
+    }
+    if (data) {
+        content = (
+            <ul className="events-list">
+                {data.map(
+                    event =>
+                        <li key={event.id}>
+                            <EventItem event={event}/>
+                        </li>)
+                }
+            </ul>
+        )
+    }
+
+    return (
+        <section className="content-section" id="all-events-section">
+            <header>
+                <h2>Find your next event!</h2>
+                <form onSubmit={handleSubmit} id="search-form">
+                    <input
+                        type="search"
+                        placeholder="Search events"
+                        ref={searchElement}
+                    />
+                    <button>Search</button>
+                </form>
+            </header>
+            {content}
+        </section>
+    );
 }
