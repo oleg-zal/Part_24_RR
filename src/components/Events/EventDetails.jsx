314aa9cc6515d58ca6e40db44ccaf7fe4ca4ff3c
Oleg Zalozhenkov <oleg.zalozhenkov@gmail.com>
2024-07-03 17:11:35 +0200

    401. Disabling Automatic Refetching After Invalidations

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -16,7 +16,8 @@ export default function EventDetails() {
         mutationFn: deleteEvent,
         onSuccess: () => {
             queryClient.invalidateQueries({
-                queryKey: ['events']
+                queryKey: ['events'],
+                refetchType: 'none'
             })
             navigate('/events');
         }
